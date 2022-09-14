20b5e9f0d140e4a7734710522011e617d8bcb835
Aherreras98 <aherrerasosa98@gmail.com>
2022-09-14 16:09:28 +0200

    Ejercicio 1

---
+++ src/com/example/Main.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 1. datos numericos
+
+        // 1.1 enteros
+        byte dato1 = 8;
+        short dato2 = 18;
+        int dato3 = 25;
+        long dato4 = 36;
+
+        // 1.2 decimales
+        float dato5 = 1.2f;
+        double dato6 = 1.19d;
+
+        // 2. texto
+        char dato7 = 'a';
+        String dato8 = "texto";
+
+        // 3. boolean
+        boolean dato9 = true;
+        boolean dato10 = false;
+
+        System.out.println(dato1);
+        System.out.println(dato2);
+        System.out.println(dato3);
+        System.out.println(dato4);
+        System.out.println(dato5);
+        System.out.println(dato6);
+        System.out.println(dato7);
+        System.out.println(dato8);
+        System.out.println(dato9);
+        System.out.println(dato10);
+    }
+}
